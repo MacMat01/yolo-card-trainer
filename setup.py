b69d38fb7ca7d7d1b1f3535829dbb562ebff0c44
macmat01 <matteo.machella01@gmail.com>
2024-11-01 10:38:11 +0100

    Update execution metadata and add function output logs
    Added detailed execution timestamps and updated execution counts for each code cell. Included stdout and stderr outputs to track the progress and warnings during dataset download, extraction, and card extraction processes.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 ﻿from setuptools import setup, find_packages
 
-setup(name='cards-object-detection', version='1.0.0', license='MIT',
-    description='A project for detecting cards using YOLOv8', author='MacMat01, SenseiBonsai2k',
+setup(name='yolo-card-trainer', version='1.0.0', license='MIT',
+    description='A project for creating dataset and training a YOLO model', author='MacMat01, SenseiBonsai2k',
     author_email='matteo.machella01@gmail.com, cristian.marinozzi1@gmail.com',
-    url='https://github.com/MacMat01/cards-object-detection', packages=find_packages(),
+    url='https://github.com/MacMat01/yolo-card-trainer', packages=find_packages(),
     install_requires=['opendatasets', 'ultralytics', 'opencv-python-headless',  # py-opencv
         'pandas', 'seaborn', 'matplotlib', 'ipykernel', 'torch', 'torchvision', 'torchaudio', 'imgaug', 'tqdm',
         'requests', 'pyzbar'], classifiers=['Development Status :: 3 - Alpha', 'Intended Audience :: Developers',
